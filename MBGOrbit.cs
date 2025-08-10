7eedf3bd184339b56fbcc7f2359fbebef580ee19
TL0SR2 <2788997379@qq.com>
2025-08-10 17:03:36 +0800

    修复漏洞

--- MBGOrbit.cs
+++ MBGOrbit.cs
@@ -93,7 +93,8 @@ namespace Assets.Scripts.Flight.Sim.MBG
         {
             try
             {
-                int n = GetPVNFromTime(time - _warpdelay, out double Multiplier, out double NTime);
+                time -= _warpdelay;
+                int n = GetPVNFromTime(time, out double Multiplier, out double NTime);
                 //return MBGMath.LinearInterpolation(MBG_PVList[n], MBG_PVList[n + 1], durationTime / _listAccuracyTime - n);
                 return MBGMath.HermiteInterpolation(MBG_PVList[n], MBG_PVList[n + 1], NTime, NTime + MBGMath.GetStepTime(Multiplier), time);
                 //Debug.Log($"TL0SR2 MBG Orbit Log -- GetPVPairFromTime -- Get Data n {n}  Multiplier {Multiplier}   PVCount {MBG_PVList.Count}  time {time}  NTime {NTime}   IntTime {NTime + MBGMath.GetStepTime(Multiplier)}  nPV PostionLength {MBG_PVList[n].Position.magnitude} VelocityLength {MBG_PVList[n].Velocity.magnitude}  n+1PV PostionLength {MBG_PVList[n+1].Position.magnitude} VelocityLength {MBG_PVList[n+1].Velocity.magnitude}  Output PostionLength {Output.Position.magnitude} VelocityLength {Output.Velocity.magnitude}");
