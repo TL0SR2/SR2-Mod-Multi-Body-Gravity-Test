48cd988642cf5448184e862819ce53ca3a80f746
TL0SR2 <2788997379@qq.com>
2025-08-14 18:57:10 +0800

    文件结构大改

---
+++ Scripts/MBGGeneralizedPlanet.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Flight.Sim.MBG
+{
+    public class MBGGeneralizedPlanet
+    {
+        
+    }
+}
